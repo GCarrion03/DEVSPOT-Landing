4304d6fc3f0513d178ab823fc110f94f5b92ff76
Gustavo Michael Carrion Lopez <gustavo.carrion@careerbuilder.com>
2020-08-03 11:11:02 -0500

    Added AWS cognito integration

--- components/menuHeader/menuHeader.js
+++ components/menuHeader/menuHeader.js
@@ -1,11 +1,23 @@
 class menuHeader extends HTMLElement {
+    accessToken = '';
+    userData;
     constructor() {
         super();
     }
     connectedCallback() {
+        this.setAccessToken();
         const div = document.createElement('div');
         div.id = "tmpDiv";
-        div.innerHTML =
+        const userMenu = this?.userData ? `<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">${this.userData['cognito:username']}<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                     <li><a href="?signout=true">Sign out</a></li>
+                                </ul>` :
+            `<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Visitor<span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                     <li><a href="https://devspot.auth.us-east-1.amazoncognito.com/login?client_id=6ua8mf7vk1eh8hqofll0ggshk3&response_type=token&scope=email+openid&redirect_uri=https://devspot.org">Sign in</a></li>
+                                     <li><a href="https://devspot.auth.us-east-1.amazoncognito.com/signup?client_id=6ua8mf7vk1eh8hqofll0ggshk3&response_type=token&scope=email+openid&redirect_uri=https://devspot.org">Sign up</a></li>
+                                </ul>`;
+            div.innerHTML =
             `<template id="menu-template">
                 <nav class="navbar navbar-fixed-top inbody">
                     <div class="container">
@@ -25,6 +37,9 @@ class menuHeader extends HTMLElement {
                                         <li><a href="SOA-C01.html">SysOps A.(SOA-C01)</a></li>
                                     </ul>
                                 </li>
+                                <li class="dropdown">
+                                    ${userMenu}
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -36,5 +51,48 @@ class menuHeader extends HTMLElement {
         document.body.removeChild(div);
         document.body.appendChild(node);
     }
+
+
+    setAccessToken() {
+        let id_token = getParameterByName('id_token');
+        if (getParameterByName('signout'))
+        {
+            window.sessionStorage.accessToken = '';
+            this.accessToken = '';
+            this.userData = '';
+            return;
+        }
+        if (id_token) {
+            window.sessionStorage.accessToken = id_token;
+            this.accessToken = id_token;
+            this.userData =  parseJwt(id_token);
+        } else {
+            id_token = window.sessionStorage.accessToken;
+            if (id_token) {
+                this.accessToken = window.sessionStorage.accessToken;
+                this.userData =  parseJwt(this.accessToken);
+            }
+        }
+    }
 }
 customElements.define('header-menu', menuHeader);
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[#?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
