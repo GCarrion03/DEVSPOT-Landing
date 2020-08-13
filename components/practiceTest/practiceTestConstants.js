2a3bbe3ba33b21b9fe72e3a08ba14ceedd2f8587
Gustavo Michael Carrion Lopez <gustavo.carrion@careerbuilder.com>
2020-08-13 16:26:02 -0500

    Added endpoints for test & prod

--- components/practiceTest/practiceTestConstants.js
+++ components/practiceTest/practiceTestConstants.js
@@ -1,11 +1,11 @@
 export let constants;
 window.location.hostname === 'devspot.org' ?
     constants = {
-        examEndpoint: 'https://wmb2ihcl2e.execute-api.us-east-1.amazonaws.com/default/exam',
-        questionEndpoint: 'https://wmb2ihcl2e.execute-api.us-east-1.amazonaws.com/default/question',
-        myTrackEndpoint: 'https://wmb2ihcl2e.execute-api.us-east-1.amazonaws.com/default/question'
-    } : constants = {
         examEndpoint: 'https://wmb2ihcl2e.execute-api.us-east-1.amazonaws.com/prod/stable/exam',
         questionEndpoint: 'https://wmb2ihcl2e.execute-api.us-east-1.amazonaws.com/prod/stable/question',
         myTrackEndpoint: 'https://wmb2ihcl2e.execute-api.us-east-1.amazonaws.com/prod/stable/mytrack'
+    } : constants = {
+        examEndpoint: 'https://wmb2ihcl2e.execute-api.us-east-1.amazonaws.com/default/exam',
+        questionEndpoint: 'https://wmb2ihcl2e.execute-api.us-east-1.amazonaws.com/default/question',
+        myTrackEndpoint: 'https://wmb2ihcl2e.execute-api.us-east-1.amazonaws.com/default/question'
     };
